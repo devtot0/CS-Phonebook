475586dc9e8d1e852c1240ccb4d6c4604dc58dfa
devtot0 <33835148+devtot0@users.noreply.github.com>
2023-03-24 20:26:44 +0100

    added Program changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string selectOption = null;
+do
+{
+    Console.WriteLine("=======PHONEBOOK=======");
+    Console.WriteLine("1 - Add a contact");
+    Console.WriteLine("2 - Display by number");
+    Console.WriteLine("3 - Display all contacts");
+    Console.WriteLine("4 - Search by name");
+    Console.WriteLine("5 - Exit");
+    Console.Write(">");
+    selectOption = Console.ReadLine();
+    if(selectOption == "5")
+    {
+        Console.WriteLine("Quitting...");
+        break;
+    }
+    switch (selectOption)
+    {
+        case "5":
+            Console.WriteLine("Quitting...");
+            break;
+    }
+} while (true);
+
